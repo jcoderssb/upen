327e41fdefd5425e46d9860de199e9ffd640a588
Hafiz Haziq <hafizhaziq.dev@gmail.com>
2022-09-28 09:43:15 +0800

    Update aside.php

--- app/Views/components/aside.php
+++ app/Views/components/aside.php
@@ -71,118 +71,116 @@ $uri = service('uri');
                         </a>
                     </li>
 
-<<<<<<< Updated upstream
-                <li class="nav-item">
-                    <a href="<?= base_url('dashboard') ?>" class="nav-link <?= $uri->getSegment(1) === 'dashboard' ? 'active' : '' ?>">
-                        <i class="nav-icon fas fa-tachometer-alt"></i>
-                        <p>Dashboard</p>
-                    </a>
-                </li>
-                <li class="nav-item <?= $uri->getSegment(1) === 'permohonan' ? 'menu-open' : '' ?>">
-                    <a href="#" class="nav-link <?= $uri->getSegment(1) === 'permohonan' ? 'active' : '' ?>">
-                        <i class="nav-icon fas fa-folder"></i>
-                        <p>
-                            Permohonan
-                            <i class="fas fa-angle-left right"></i>
-                        </p>
-                    </a>
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="<?= base_url('permohonan') ?>" class="nav-link <?= uri_string() === 'permohonan' ? 'active' : '' ?>">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Senarai Permohonan</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="<?= base_url('permohonan/daftar') ?>" class="nav-link <?= uri_string() === 'permohonan/daftar' ? 'active' : '' ?>">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Daftar Permohonan</p>
-                            </a>
-                        </li>
-						<li class="nav-item">
-                            <a href="<?= base_url('permohonan/mmk') ?>" class="nav-link <?= uri_string() === 'permohonan/mmk' ? 'active' : '' ?>">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Senarai Status MMK</p>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-folder"></i>
-                        <p>
-                            Kutipan
-                            <i class="fas fa-angle-left right"></i>
-                        </p>
-                    </a>
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="<?= base_url('kutipan') ?>" class="nav-link <?= uri_string() === 'kutipan' ? 'active' : '' ?>">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Senarai Kutipan</p>
-                            </a>
-                        </li>
-						 <li class="nav-item">
-                            <a href="#" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Pemulangan Deposit</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="#" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Cetakan Resit</p>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-folder"></i>
-                        <p>
-                            Perakaunan
-                            <i class="fas fa-angle-left right"></i>
-                        </p>
-                    </a>
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="#" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Senarai Perakaunan</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="https://teyadashawty.bukku.my/dashboard" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Modul Perakaunan</p>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-folder"></i>
-                        <p>
-                            Pentadbir
-                            <i class="fas fa-angle-left right"></i>
-                        </p>
-                    </a>
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="#" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Senarai Pentadbir</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="#" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Daftar Pentadbir</p>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-=======
+                    <li class="nav-item">
+                        <a href="<?= base_url('dashboard') ?>" class="nav-link <?= $uri->getSegment(1) === 'dashboard' ? 'active' : '' ?>">
+                            <i class="nav-icon fas fa-tachometer-alt"></i>
+                            <p>Dashboard</p>
+                        </a>
+                    </li>
+                    <li class="nav-item <?= $uri->getSegment(1) === 'permohonan' ? 'menu-open' : '' ?>">
+                        <a href="#" class="nav-link <?= $uri->getSegment(1) === 'permohonan' ? 'active' : '' ?>">
+                            <i class="nav-icon fas fa-folder"></i>
+                            <p>
+                                Permohonan
+                                <i class="fas fa-angle-left right"></i>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="<?= base_url('permohonan') ?>" class="nav-link <?= uri_string() === 'permohonan' ? 'active' : '' ?>">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Senarai Permohonan</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="<?= base_url('permohonan/daftar') ?>" class="nav-link <?= uri_string() === 'permohonan/daftar' ? 'active' : '' ?>">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Daftar Permohonan</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="<?= base_url('permohonan/mmk') ?>" class="nav-link <?= uri_string() === 'permohonan/mmk' ? 'active' : '' ?>">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Senarai Status MMK</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="nav-icon fas fa-folder"></i>
+                            <p>
+                                Kutipan
+                                <i class="fas fa-angle-left right"></i>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="<?= base_url('kutipan') ?>" class="nav-link <?= uri_string() === 'kutipan' ? 'active' : '' ?>">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Senarai Kutipan</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Pemulangan Deposit</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Cetakan Resit</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="nav-icon fas fa-folder"></i>
+                            <p>
+                                Perakaunan
+                                <i class="fas fa-angle-left right"></i>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Senarai Perakaunan</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="https://teyadashawty.bukku.my/dashboard" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Modul Perakaunan</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="nav-icon fas fa-folder"></i>
+                            <p>
+                                Pentadbir
+                                <i class="fas fa-angle-left right"></i>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Senarai Pentadbir</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Daftar Pentadbir</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
                     <li class="nav-item">
                         <a href="#" class="nav-link">
                             <i class="nav-icon fas fa-tachometer-alt"></i>
@@ -261,7 +259,6 @@ $uri = service('uri');
                         </ul>
                     </li>
                 <?php endif ?>
->>>>>>> Stashed changes
             </ul>
         </nav>
     </div>
