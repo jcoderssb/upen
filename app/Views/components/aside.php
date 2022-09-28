444b33c33299b6f2154a9c92dcfc11d581db1b94
Hafiz Haziq <hafizhaziq.dev@gmail.com>
2022-09-28 08:56:40 +0800

    Update aside.php

--- app/Views/components/aside.php
+++ app/Views/components/aside.php
@@ -4,15 +4,15 @@ $uri = service('uri');
 
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
 
-    <a href="index3.html" class="brand-link">
-        <img src="<?= base_url('adminlte/dist/img/AdminLTELogo.png') ?>" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">UPEN</span>
+    <a href="<?= base_url() ?>" class="brand-link">
+        <img src="<?= base_url('img/perumahan/logoN9.png') ?>" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light">ePENS</span>
     </a>
 
     <div class="sidebar">
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="<?= base_url('adminlte/dist/img/user2-160x160.jpg') ?>" class="img-circle elevation-2" alt="User Image">
+                <img src="<?= base_url('https://unsplash.it/id/1/300/300') ?>" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
                 <a href="#" class="d-block"><?= session()->get("username") ?></a>
@@ -21,7 +21,57 @@ $uri = service('uri');
 
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                <!-- admin -->
+                <?php if (session()->get("peranan") == 1) : ?>
+                    <li class="nav-item">
+                        <a href="<?= base_url('dashboard') ?>" class="nav-link <?= $uri->getSegment(1) === 'dashboard' ? 'active' : '' ?>">
+                            <i class="nav-icon fas fa-tachometer-alt"></i>
+                            <p>Dashboard</p>
+                        </a>
+                    </li>
+                    <li class="nav-item <?= $uri->getSegment(1) === 'pengguna' ? 'menu-open' : '' ?>">
+                        <a href="" class="nav-link <?= $uri->getSegment(1) === 'pengguna' ? 'active' : '' ?>">
+                            <i class="nav-icon fas fa-users"></i>
+                            <p>
+                                Pengurusan Pengguna
+                                <i class="fas fa-angle-left right"></i>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item ">
+                                <a href="<?= base_url('pengguna') ?>" class="nav-link <?= $uri->getSegment(1) === 'pengguna' ? 'active' : '' ?>">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Senarai Pengguna</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="nav-icon fas fa-chart-line"></i>
+                            <p>Statistik & Laporan</p>
+                        </a>
+                    </li>
+                    <li class="nav-item ">
+                        <a href="<?= base_url('audit_trails') ?>" class="nav-link <?= $uri->getSegment(1) === 'audit_trails' ? 'active' : '' ?>">
+                            <i class="nav-icon fas fa-book"></i>
+                            <p>Jejak Audit</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="nav-icon fas fa-tachometer-alt"></i>
+                            <p>Pengurusan pengumuman/aktiviti BP</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="nav-icon fas fa-tachometer-alt"></i>
+                            <p>Kemasukan Kod Kewangan</p>
+                        </a>
+                    </li>
 
+<<<<<<< Updated upstream
                 <li class="nav-item">
                     <a href="<?= base_url('dashboard') ?>" class="nav-link <?= $uri->getSegment(1) === 'dashboard' ? 'active' : '' ?>">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
@@ -132,6 +182,86 @@ $uri = service('uri');
                         </li>
                     </ul>
                 </li>
+=======
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="nav-icon fas fa-tachometer-alt"></i>
+                            <p>Penyelenggaraan Kod Sistem</p>
+                        </a>
+                    </li>
+                <?php endif ?>
+
+                <!-- pegawai -->
+                <?php if (session()->get("peranan") == 2) : ?>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="nav-icon fas fa-folder"></i>
+                            <p>
+                                Kutipan
+                                <i class="fas fa-angle-left right"></i>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Senarai Kutipan</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Daftar Kutipan</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="nav-icon fas fa-folder"></i>
+                            <p>
+                                Perakaunan
+                                <i class="fas fa-angle-left right"></i>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Senarai Perakaunan</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Daftar Perakaunan</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                <?php endif ?>
+
+                <!-- pemohon -->
+                <?php if (session()->get("peranan") == 3) : ?>
+                    <li class="nav-item <?= $uri->getSegment(1) === 'permohonan' ? 'menu-open' : '' ?>">
+                        <a href="#" class="nav-link <?= $uri->getSegment(1) === 'permohonan' ? 'active' : '' ?>">
+                            <i class="nav-icon fas fa-folder"></i>
+                            <p>
+                                Permohonan
+                                <i class="fas fa-angle-left right"></i>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="<?= base_url('permohonan') ?>" class="nav-link <?= uri_string() === 'permohonan' ? 'active' : '' ?>">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Senarai Permohonan</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                <?php endif ?>
+>>>>>>> Stashed changes
             </ul>
         </nav>
     </div>
